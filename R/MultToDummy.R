7bcad8485c34c743bc65051f4eca3ef778213161
Md Ahsanul Islam <67047779+MdAhsanulHimel@users.noreply.github.com>
2024-09-30 23:20:00 +0600

    edited regex example code

--- R/MultToDummy.R
+++ R/MultToDummy.R
@@ -10,37 +10,37 @@
 #' MultToDummy(x, splitby = ";")
 #'
 #' # Example for split by capital letters:
-#' MultToDummy(x, splitby = "(?<=[A-Z])|(?=[A-Z])")
-#' 
+#' MultToDummy(x, splitby = "(?<=.)(?=[A-Z])")
+#'
 MultToDummy <- function(x, splitby = ";", bind = TRUE) {
-  
+
   # splits the multiple responses
-  x_split <- strsplit(x, splitby, perl = TRUE) 
+  x_split <- strsplit(x, splitby, perl = TRUE)
   x_split <- lapply(x_split, function(x){
     # removes commas + space if they are at the end of the string
-    x <- gsub(",\\s?$", "", x)   
+    x <- gsub(",\\s?$", "", x)
     return(x)
   })
-  
+
   # take the unique responses
   cols <- unique(unlist(x_split))
-  
+
   # make dummy variables for each column by rows using sapply
   dummy <- t(sapply(x_split,
                     function(x) {
                       table(factor(x, levels = cols))
-                    }))  
-  
+                    }))
+
   # column names
   actual <- paste(substitute(x))[3]
-  
+
   # construction of final dataset
   fin_df <-  cbind(MultResponse = x, data.frame(dummy))
-  
+
   # specifying column names
-  colnames(fin_df) <- c(substitute(actual), 
+  colnames(fin_df) <- c(substitute(actual),
                         paste(actual, gsub("[ -]", "_", cols), sep="."))
-  
+
   # If bind==True, return the actual variable along with the dummy variables
   # If bind==False, only return the dummy variables
   ifelse(bind==TRUE,
